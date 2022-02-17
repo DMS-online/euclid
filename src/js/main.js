a2fa290817914905c886fd62ab3eeea804edf7c2
SineYlo <vavshhik01@mail.ru>
2022-02-17 13:53:23 +1000

    info: the template is set up and ready to write code

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-unused-vars */
 // |=============== CONNECTING EXTERNAL MODULES ===============>
-// import module_name from 'module-name';
+import focusVisible from 'focus-visible';
 
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
-// |=============== SCRIPT CONNECTION |  ===============>
+// |=============== SCRIPT CONNECTION | BURGER ===============>
+import burger from './components/home/burger';
+// |=============== SCRIPT CONNECTION | SEARCH ===============>
+import search from './components/home/search';
+// |=============== SCRIPT CONNECTION | SWIPER ===============>
+import slider from './components/home/slider';
+// |=============== SCRIPT CONNECTION | TABS ===============>
+import tab from './components/home/tab';
+// |=============== SCRIPT CONNECTION | ACCORDION ===============>
+import accordion from './components/home/accordion';
+// |=============== SCRIPT CONNECTION | SUPER PLACEHOLDER ===============>
+import placeholder from './components/home/placeholder';
+// |=============== SCRIPT CONNECTION | PICTURE ===============>
+import picture from './components/home/picture';
